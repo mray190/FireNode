d89745f81dcf6a9b6137e5fccf105e062219e044
sjacker <sjacker@umich.edu>
2016-02-20 15:42:04 -0500

    fixing fragment lifecycle

--- app/src/main/java/co/hmika/firenode/Home.java
+++ app/src/main/java/co/hmika/firenode/Home.java
@@ -176,14 +176,19 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
         if (id == R.id.live_view) {
             MapsFragment fragment = new MapsFragment();
-            currFragment = fragment;
-            fragmentTransaction.replace(R.id.main_frag, fragment);
+            Fragment oldFragment = getSupportFragmentManager().findFragmentById(R.id.main_frag);
+            if(fragment != null) {
+                fragmentTransaction.remove(oldFragment);}
+        currFragment = fragment;
+            fragmentTransaction.add(R.id.main_frag, fragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         }
         else if (id == R.id.debug) {
             DebugFragment fragment = new DebugFragment();
             currFragment = fragment;
             fragmentTransaction.replace(R.id.main_frag, fragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         }
 
