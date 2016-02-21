566e12ab61392fa8921a93e5dab3229b4a8f2194
Michael Ray <mray190@gmail.com>
2016-02-20 20:29:30 -0500

    Push Notifications

---
+++ app/src/main/java/co/hmika/firenode/CustomNotification.java
@@ -0,0 +1,28 @@
+package co.hmika.firenode;
+
+/**
+ * Created by mray on 2/20/16.
+ */
+public class CustomNotification {
+    private String title;
+    private String text;
+    public CustomNotification() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/co/hmika/firenode/FirebaseManager.java
+++ app/src/main/java/co/hmika/firenode/FirebaseManager.java
@@ -1,6 +1,12 @@
 package co.hmika.firenode;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
@@ -14,16 +20,61 @@ public class FirebaseManager {
     private FireNode fireNode;
     private Firebase ref;
 
+    public void notifyUser(String notificationTitle, String notificationText) {
+        Intent intent = new Intent(fireNode, Home.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(fireNode, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder b = new NotificationCompat.Builder(fireNode);
+
+        b.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
+                .setTicker("FireNode")
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationText)
+                .setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent)
+                .setContentInfo("FireNode");
+
+
+        NotificationManager notificationManager = (NotificationManager) fireNode.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, b.build());
+    }
+
     public FirebaseManager(final FireNode fireNode0) {
         this.fireNode = fireNode0;
         ref = new Firebase("https://firenodemhacks.firebaseio.com/");
+        ref.child("notifications").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                CustomNotification customNotification = dataSnapshot.getValue(CustomNotification.class);
+                notifyUser(customNotification.getTitle(),customNotification.getText());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                CustomNotification customNotification = dataSnapshot.getValue(CustomNotification.class);
+                notifyUser(customNotification.getTitle(),customNotification.getText());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) { }
+        });
         ref.child("parse_data").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Router new_router = dataSnapshot.getValue(Router.class);
                 new_router.setBssid(dataSnapshot.getKey());
-                if (new_router.getGps_lat()==0 || new_router.getGps_lon()==0 || new_router.getRange()==0) return;
-                if (fireNode.map!=null) {
+                if (new_router.getGps_lat() == 0 || new_router.getGps_lon() == 0 || new_router.getRange() == 0)
+                    return;
+                if (fireNode.map != null) {
                     new_router.setCircle(fireNode.map.addCircle(new CircleOptions().center(new LatLng(new_router.getGps_lat(),
                             new_router.getGps_lon())).radius(new_router.getRange())
                             .strokeColor(Color.GREEN)
@@ -37,8 +88,9 @@ public class FirebaseManager {
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 Router new_router = dataSnapshot.getValue(Router.class);
                 new_router.setBssid(dataSnapshot.getKey());
-                if (new_router.getGps_lat()==0 || new_router.getGps_lon()==0 || new_router.getRange()==0) return;
-                if (fireNode.map!=null) {
+                if (new_router.getGps_lat() == 0 || new_router.getGps_lon() == 0 || new_router.getRange() == 0)
+                    return;
+                if (fireNode.map != null) {
                     new_router.setCircle(fireNode.router_list.get(new_router.getBssid()).getCircle());
                     new_router.getCircle().setRadius(new_router.getRange());
                     new_router.getCircle().setFillColor(Color.parseColor("#3300ff00"));
@@ -61,10 +113,12 @@ public class FirebaseManager {
             }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
 
             @Override
-            public void onCancelled(FirebaseError firebaseError) { }
+            public void onCancelled(FirebaseError firebaseError) {
+            }
         });
     }
 
